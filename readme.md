68419cb8507fb177600b7f380493fb831e3f4f2d
Paul Williamson <pwilliamson@mimecast.com>
2024-07-09 14:25:18 +0100

    feat: initial commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Potato Plant Disease Classification
+
+
+## Dataset Setup
+1. Download the dataset
+```bash
+pip install -r requirements.txt
+kaggle datasets download -d hafiznouman786/potato-plant-diseases-data
+```
+2. Unzip into your root dir of the project
+3. Run the data preparation script
+```bash
+python utils/dataset_setup.py
+```
---
+++ utils/dataset_setup.py
@@ -0,0 +1,31 @@
+import random
+import os
+import pandas as pd
+
+data_dir = "potato-plant-diseases-data/PlantVillage/PlantVillage"
+
+if __name__=="__main__":
+    
+    classes = os.listdir(data_dir)
+
+    dataset = pd.DataFrame(columns=["image", "label"])
+    for i, c in enumerate(classes):
+        images = os.listdir(os.path.join(data_dir, c))
+        dataset = pd.concat(
+            [
+                dataset, 
+                pd.DataFrame({"image": images, "label": [i] * len(images)})
+            ], 
+            axis=0
+        ).reset_index(drop=True)
+
+    dataset["split"] = None
+    idxs = dataset.index.tolist()
+    random.shuffle(idxs)
+    n = len(idxs)
+    train_size = int(0.8 * n)
+    test_size = n - train_size
+    dataset.loc[idxs[:train_size], "split"] = "train"
+    dataset.loc[idxs[train_size:], "split"] = "test"
+
+    dataset.to_csv(f"{data_dir}/dataset_metadata.csv", index=False)
