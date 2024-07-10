f44d3d724688523897545254d503d0de5c1921f2
Paul Williamson <pwilliamson@mimecast.com>
2024-07-10 11:16:59 +0100

    feat: main now has hpo

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
-import os
-import time
-
 import argparse
 
-from src.training import setup_train
-from utils.decorators import inject_config_single_model_train
-
-def check_dir_and_subdir_exists(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-def create_paths():
-    now = time.strftime("%Y-%m-%d-%H-%M-%S")
-    new_save_path = f"model_saves/resnet/{now}"
-    check_dir_and_subdir_exists(new_save_path)
-
-    logging_path = f"logs/model_training.csv"
-    check_dir_and_subdir_exists("logs")
-    return new_save_path, logging_path
-
-@inject_config_single_model_train()
-def train_one_model(config:dict):
-    new_save_path, logging_path = create_paths()
-    config["save_location"] = new_save_path
-    config["training_log"] = logging_path
-    trainer = setup_train(config)
-    trainer.train()
+from src.training import hpo_train, train_one_model
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--hpo', 
         action='store_true', 
-        help='Whether to run hyperparameter optimization or single model train'
+        help='Whether to run hyperparameter optimization (True) or single model train (False, default)'
     )
     args = parser.parse_args()
 
     if args.hpo:
-        raise NotImplementedError("Hyperparameter optimization not yet implemented")
+        hpo_train()
     else:
         train_one_model()
 
