fc152f81788c63b5bfd68bb685d42e76b00420a9
Paul Williamson <pwilliamson@mimecast.com>
2024-07-10 11:16:22 +0100

    feat: add hpo config decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,10 +1,11 @@
+import os
 import yaml
 
 def load_config(yaml_file_path):
     with open(yaml_file_path, 'r') as file:
         return yaml.safe_load(file)
 
-def inject_config_params(*param_names, config_path='config.yaml'):
+def inject_config_params(*param_names, config_path=os.path.abspath('config.yaml')):
     def decorator(func):
         def wrapper(*args, **kwargs):
             config = load_config(config_path)
@@ -13,11 +14,10 @@ def inject_config_params(*param_names, config_path='config.yaml'):
         return wrapper
     return decorator
 
-def inject_config_single_model_train(config_path='model_train_config.yaml'):
+def inject_train_config(config_path=os.path.abspath('model_train_config.yaml')):
     def decorator(func):
         def wrapper(*args, **kwargs):
             config = load_config(config_path)
-            # injected_params = {param: config[param] for param in param_names if param in config}
             return func(config)
         return wrapper
     return decorator
