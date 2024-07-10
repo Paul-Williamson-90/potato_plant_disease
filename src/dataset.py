4c863e93b23d1d860f9872dd1a325417f26101b4
Paul Williamson <pwilliamson@mimecast.com>
2024-07-10 11:16:08 +0100

    tidy: remove dataset test code

--- src/dataset.py
+++ src/dataset.py
@@ -74,27 +74,3 @@ def dataset_factory(
     dataset = PotatoDataset(image_meta_path, image_data_path, split, transform)
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return dataset, data_loader
-
-
-
-if __name__=="__main__":
-    image_meta_path = "potato-plant-diseases-data/PlantVillage/dataset_metadata.csv"
-    image_data_path = "potato-plant-diseases-data/PlantVillage/PlantVillage"
-    split = "train"
-
-    transform = ImageTransform(
-        output_size=[255, 255], rescale=True, random_crop=0.2, to_tensor=True
-    )
-    dataset = PotatoDataset(image_meta_path, image_data_path, split, transform)
-    print(len(dataset))
-    for i in range(10):
-        sample = dataset[i]
-        print(sample['image'].shape)
-        print(sample['label'])
-        print("===")
-
-    _, data_loader = dataset_factory(image_meta_path, image_data_path, split, transform=transform)
-    for i, batch in enumerate(data_loader):
-        print(batch["image"][0].shape)
-        print(batch["label"])
-        break
